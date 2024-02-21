b517d1f1010cd782763a1d8fb753e864f65dcc01
amine <amineparis97x@gmail.com>
2024-02-21 12:54:42 +0100

    Adding redux-persist for Redux state persistence, to retain user data upon page reload.

--- frontend/src/redux/store.jsx
+++ frontend/src/redux/store.jsx
@@ -1,12 +1,28 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { userReducer } from './reducers/userSlice';
 import { userProfileReducer } from './reducers/userProfileSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; 
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['user', 'userProfile'], 
+};
 
 const rootReducer = combineReducers({
     user: userReducer,
     userProfile: userProfileReducer,
 });
 
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
 });
+
+
+export const persistor = persistStore(store);
